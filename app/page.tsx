975b61a51741f71b624b319a82b70b0485d2fd2c
Ahmed <ahmaat19@gmail.com>
2024-02-14 11:42:40 +0300

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <Button>Click me</Button>
+      </div>
+    </section>
+  )
+}
